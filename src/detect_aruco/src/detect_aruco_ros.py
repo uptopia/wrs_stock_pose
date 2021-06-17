3fe8025b12d7c10c72a2c3f3cc4c07e0a69178c0
upup <charlotte07221@gmail.com>
2021-06-17 19:50:46 +0800

    get realsense intrinsic parameter

--- src/detect_aruco/src/detect_aruco_ros.py
+++ src/detect_aruco/src/detect_aruco_ros.py
@@ -29,7 +29,8 @@
 
 import rospy
 from std_msgs.msg import String, Float64MultiArray
-from sensor_msgs.msg import Image as msg_Image
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import CameraInfo
 
 import cv2
 import cv2.aruco as aruco
@@ -100,95 +101,89 @@ import numpy as np
 
 # distortion_coefficients = np.array([k_1, k_2, p_1, p_2, k_3])
 
-def publish_aruco_marker_corners(corners):              
-    corners_list = []
-    for num in range(0, len(corners)):
-        corner = corners[num][0]
+class ArUcoMarkerPosture():
+    def __init__(self):
+        self.sub_camera_info = rospy.Subscriber('/camera/color/camera_info', CameraInfo, self.get_camera_info)    
+        self.sub_markers = rospy.Subscriber('/camera/color/image_raw', Image, self.detect_aruco_markers)
         
-        for angle in range(0, len(corner)):                
-            corners_list.append(corner[angle][0]) # angle.pixel_x
-            corners_list.append(corner[angle][1]) # angle.pixel_y
-    
-    corners_ros_msg = Float64MultiArray()
-    corners_ros_msg.data = corners_list
-    # rospy.loginfo(corners_ros_msg)     
-    pub_corners.publish(corners_ros_msg)
-    
+        self.pub_corners = rospy.Publisher('aruco_corners', Float64MultiArray, queue_size = 1)
+                
+    def get_camera_info(self, camera_info):
+        self.height = camera_info.height
+        self.width = camera_info.width
+        self.frame_id = camera_info.header.frame_id        
+        self.intrinsic_matrix = np.array(camera_info.K).reshape(3, 3)
+        self.distortion_coefficients = np.array(camera_info.D)
 
+    def detect_aruco_markers(self, data):
+        global corners
+        # Constant parameters used in Aruco methods
+        markerLength = 0.02
+        ARUCO_PARAMETERS = aruco.DetectorParameters_create()
+        ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_6X6_1000)
 
-def detect_aruco_markers(data):
-    c_x = 326.004974365234 #PPX
-    c_y = 237.414825439453 #PPY
-    f_x = 615.178771972656 #FX
-    f_y = 615.507995605469 #FY
+        bridge = CvBridge()
+        try:
+            cv_image = bridge.imgmsg_to_cv2(data, "bgr8")#data.encoding)
+        except CvBridgeError as e:
+            print(e)
+            return
 
-    k_1 = 0.0
-    k_2 = 0.0
-    p_1 = 0.0
-    p_2 = 0.0
-    k_3 = 0.0
+        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        
+        # Detect Aruco markers
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, ARUCO_DICT, parameters = ARUCO_PARAMETERS)
 
-    intrinsic_matrix = np.array([[f_x, 0.0, c_x],
-                        [0.0, f_y, c_y],
-                        [0.0, 0.0, 1.0]])
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        if ids is not None:
+            self.publish_aruco_marker_corners(corners)
 
-    distortion_coefficients = np.array([k_1, k_2, p_1, p_2, k_3])
+            ### NOTICE: some Opencv version use 2 output "rvec, tvec", others use 3 output "rvec, tvec, _"!!!!
+            rvec, tvec = aruco.estimatePoseSingleMarkers(corners, markerLength, self.intrinsic_matrix, self.distortion_coefficients)
+            # (rvec - tvec).any()  # get rid of that nasty numpy value array error
 
-    global corners
-    # Constant parameters used in Aruco methods
-    markerLength = 0.02
-    ARUCO_PARAMETERS = aruco.DetectorParameters_create()
-    ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_6X6_1000)
+            # Print corners and ids to the console
+            for i, corner in zip(ids, corners):
+                print('ID: {};\nCorners:\n{}'.format(i, corner))    
 
-    bridge = CvBridge()
-    try:
-        cv_image = bridge.imgmsg_to_cv2(data, "bgr8")#data.encoding)
-    except CvBridgeError as e:
-        print(e)
-        return
+            strg = ''
+            for n in range(0, ids.size):
+                strg += str(ids[n][0])+', '
+                aruco.drawAxis(cv_image, self.intrinsic_matrix, self.distortion_coefficients, rvec[n], tvec[n], 0.01)
 
-    gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-    
-    # Detect Aruco markers
-    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, ARUCO_DICT, parameters = ARUCO_PARAMETERS)
+            # Outline all of the markers detected in our image
+            aruco.drawDetectedMarkers(cv_image, corners, ids, borderColor = (0, 0, 255))
+            cv2.putText(cv_image, "Id: " + strg, (10,30), font, 1, (0,0,255), 2, cv2.LINE_AA)
 
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    if ids is not None:
-        publish_aruco_marker_corners(corners)
+        else:
+            # code to show 'No Ids' when no markers are found
+            cv2.putText(cv_image, "No Ids", (10,30), font, 1, (0,0,255), 2, cv2.LINE_AA)
 
-        ### NOTICE: some Opencv version use 2 output "rvec, tvec", others use 3 output "rvec, tvec, _"!!!!
-        rvec, tvec = aruco.estimatePoseSingleMarkers(corners, markerLength, intrinsic_matrix, distortion_coefficients)
-        # (rvec - tvec).any()  # get rid of that nasty numpy value array error
+        cv2.imshow('cv_image', cv_image)
+        cv2.waitKey(1)
 
-        # Print corners and ids to the console
-        for i, corner in zip(ids, corners):
-            print('ID: {};\nCorners:\n{}'.format(i, corner))    
+    def publish_aruco_marker_corners(self, corners):              
+        corners_list = []
+        for num in range(0, len(corners)):
+            corner = corners[num][0]
+            
+            for angle in range(0, len(corner)):                
+                corners_list.append(corner[angle][0]) # angle.pixel_x
+                corners_list.append(corner[angle][1]) # angle.pixel_y
+        
+        corners_ros_msg = Float64MultiArray()
+        corners_ros_msg.data = corners_list
+        # rospy.loginfo(corners_ros_msg)     
+        self.pub_corners.publish(corners_ros_msg)
 
-        strg = ''
-        for n in range(0, ids.size):
-            strg += str(ids[n][0])+', '
-            aruco.drawAxis(cv_image, intrinsic_matrix, distortion_coefficients, rvec[n], tvec[n], 0.01)
-
-        # Outline all of the markers detected in our image
-        aruco.drawDetectedMarkers(cv_image, corners, ids, borderColor = (0, 0, 255))
-        cv2.putText(cv_image, "Id: " + strg, (10,30), font, 1, (0,0,255), 2, cv2.LINE_AA)
-
-    else:
-        # code to show 'No Ids' when no markers are found
-        cv2.putText(cv_image, "No Ids", (10,30), font, 1, (0,0,255), 2, cv2.LINE_AA)
-
-    cv2.imshow('cv_image', cv_image)
-    cv2.waitKey(1)
 
 if __name__ == '__main__':
 
     rospy.init_node("detect_aruco_markers")
-    # try:
-    #     talker()
-    # except rospy.ROSInterruptException:
-    #     pass
-    pub_corners = rospy.Publisher('aruco_corners', Float64MultiArray, queue_size = 1)
-    sub_markers = rospy.Subscriber('/camera/color/image_raw', msg_Image, detect_aruco_markers)
-    
-    #rate = rospy.Rate(10) # 10hz
+
+    try:
+        ArUcoMarkerPosture()
+    except rospy.ROSInterruptException:
+        pass
+
     rospy.spin()
